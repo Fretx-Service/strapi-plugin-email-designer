b86343d5c31fc31aa9dc0bb09336732cde2ca126
Jonathan Muribeca <jonathanmuribeca@gmail.com>
2021-10-04 17:07:29 -0300

    Creates sendBulkTemplatedEmail function for mailjet

--- services/email.js
+++ services/email.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const isValidEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const decode = require('decode-html');
 const { htmlToText } = require('html-to-text');
+const mailjet = require ('node-mailjet');
 
 /**
  * fill subject, text and html using lodash template
@@ -33,18 +34,18 @@ const sendTemplatedEmail = async (emailOptions = {}, emailTemplate = {}, data = 
   if (missingAttributes.length > 0) {
     throw new Error(`Following attributes are missing from your email template : ${missingAttributes.join(', ')}`);
   }
-  
+
   let queryT = {};
   if(emailTemplate.templateId){
     queryT = { id: emailTemplate.templateId };
   }else if( emailTemplate.templateName){
     queryT = { name: emailTemplate.templateName };
   }
-       
+
   let { bodyHtml, bodyText } = await strapi
   .query('email-template', 'email-designer')
   .findOne(queryT);
-  
+
   if ((!bodyText || !bodyText.length) && bodyHtml && bodyHtml.length)
     bodyText = htmlToText(bodyHtml, { wordwrap: 130, trimEmptyLines: true });
 
@@ -63,6 +64,119 @@ const sendTemplatedEmail = async (emailOptions = {}, emailTemplate = {}, data = 
   );
 
   return strapi.plugins.email.provider.send({ ...emailOptions, ...templatedAttributes });
+};
+
+/**
+ * Just support mailjet provider !
+ * fill subject, text and html using lodash template
+ * @param {object} emailTemplate - object containing attributes to fill
+ * @param {array} data - data used to fill the template. data.emailOptions.to is required.(optional emailOptions itens: from and replyto...)
+ * @returns {{ response }}
+ */
+const sendBulkTemplatedEmail = async (emailTemplate = {}, data = []) => {
+  if (_.isEmpty(data) || _.isEmpty(emailTemplate)) throw new Error('Invalid parameters!');
+  const requiredAttributes = ['templateName', 'subject'];
+  const attributes = [...requiredAttributes, 'text', 'html'];
+  const missingAttributes = _.difference(requiredAttributes, Object.keys(emailTemplate));
+  if (missingAttributes.length > 0) {
+    throw new Error(`Following attributes are missing from your email template : ${missingAttributes.join(', ')}`);
+  }
+
+  let queryT = {};
+  if(emailTemplate.templateId){
+    queryT = { id: emailTemplate.templateId };
+  }else if( emailTemplate.templateName){
+    queryT = { name: emailTemplate.templateName };
+  }
+
+  let { bodyHtml, bodyText, externalId, profile } = await strapi
+  .query('email-template', 'email-designer')
+  .findOne(queryT);
+  externalId = _.parseInt(externalId);
+  const emailSettings = (_.isEmpty(profile))
+    ? strapi.config.get('plugins.templatedEmail.default','')
+    : strapi.config.get(`plugins.templatedEmail.${profile}`,'');
+  if(emailSettings.provider !== 'mailjet') throw new Error(`Provider "${emailSettings.provider}" not supported!`);
+
+  if ((!bodyText || !bodyText.length) && bodyHtml && bodyHtml.length)
+    bodyText = htmlToText(bodyHtml, { wordwrap: 130, trimEmptyLines: true });
+
+  let messages = [];
+  data.forEach(item => {
+    const { emailOptions } = item;
+    Object.entries(emailOptions).forEach(([key, address]) => {
+      if (Array.isArray(address)) {
+        address.forEach((email) => {
+          if (!isValidEmail.test(email)) throw new Error(`Invalid "${key}" email address with value "${email}"`);
+        });
+      } else {
+        if (!isValidEmail.test(address)) throw new Error(`Invalid "${key}" email address with value "${address}"`);
+      }
+    });
+
+    // in case of externalId, uses it, else proccess local template
+    if(externalId > 0) {
+      messages.push({
+        "From": {
+          "Email": emailSettings.settings.defaultFrom,
+          "Name": emailSettings.settings.defaultFromName
+        },
+        "To": [
+          {
+            "Email": emailOptions.to,
+            "Name": ""
+          }
+        ],
+        "Subject": emailTemplate.subject,
+        "TemplateID": externalId,
+        "TemplateLanguage": true,
+        "Variables": {
+          "data" : { ...item }
+        }
+      })
+    } else {
+      let contentTemplate = {
+        html: decode(bodyHtml),
+        text: decode(bodyText),
+      };
+      const templatedAttributes = attributes.reduce(
+        (compiled, attribute) =>
+        contentTemplate[attribute]
+            ? Object.assign(compiled, { [attribute]: _.template(contentTemplate[attribute])(item) })
+            : compiled,
+        {}
+      );
+      messages.push({
+        "From": {
+          "Email": emailSettings.settings.defaultFrom,
+          "Name": emailSettings.settings.defaultFromName
+        },
+        "To": [
+          {
+            "Email": emailOptions.to,
+            "Name": ""
+          }
+        ],
+        "Subject": emailTemplate.subject,
+        "TextPart": templatedAttributes['text'],
+        "HTMLPart": templatedAttributes['html']
+      });
+    }
+  });
+  const request = mailjet
+    .connect(emailSettings.providerOptions.publicApiKey , emailSettings.providerOptions.secretApiKey)
+    .post("send", {'version': 'v3.1'})
+    .request({ "Messages": [...messages] });
+  request
+    .then((result) => {
+      return result.body;
+    })
+    .catch((err) => {
+      console.log(err.statusCode);
+      return err.statusCode;
+    });
+
+
 };
 
 /**
@@ -122,6 +236,7 @@ const send = async ({ templateName, data, to, from, replyTo, bcc, subject }) => 
 
 module.exports = {
   sendTemplatedEmail,
+  sendBulkTemplatedEmail,
   compose,
   send,
 };
